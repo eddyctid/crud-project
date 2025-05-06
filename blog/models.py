bc34b74352abe84211ac386fe04cb74a6ad2334a
eddyctid <aniketd696@gmail.com>
2025-05-06 14:40:14 +0530

    tests and static added

--- blog/models.py
+++ blog/models.py
@@ -14,5 +14,5 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self): # new
-        return reverse("post_detail", kwargs={"pk": self.pk})
+        return reverse("blog_detail_view", kwargs={"pk": self.pk})
 
--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='BlogListView' ),
-    path('blog/<int:pk>', BlogDetailView.as_view(), name='post_detail' ),
+    path('', BlogListView.as_view(), name='blog_list_view' ),
+    path('blogpost/<int:pk>/', BlogDetailView.as_view(), name='blog_detail_view'  ),
 ]
