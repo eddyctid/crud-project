e245cf3f9646c39ff9949206fa24f1f42f53912f
eddyctid <aniketd696@gmail.com>
2025-05-06 23:14:47 +0530

    Sign Up added

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',#added
+    'accounts.apps.AccountsConfig',#added
 ]
 
 MIDDLEWARE = [
