c0a5cece0c99548acb00395c4c6fc8ed56ec7572
eddyctid <aniketd696@gmail.com>
2025-05-06 23:18:04 +0530

    static collected

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -118,7 +118,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static'] # new
-
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
+STORAGES = {
+"default": {
+    "BACKEND": "django.core.files.storage.FileSystemStorage",
+},
+"staticfiles": {
+    "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+},
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
